af37c137f5a2b880da80769196df5f5dde5fd3b8
Rafaella de Olivio <rafaellaolivio071@gmail.com>
2025-07-08 19:45:22 -0300

    Implementando a lógica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -47,6 +47,10 @@ namespace xadrez_console
                         Console.ReadLine();
                     }
                 }
+
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+
             }
             catch (TabuleiroException e)
             {
